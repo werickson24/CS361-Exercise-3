13dd955af6b3bcef164eeafc8482f3e2883b0374
Wil Erickson <william@ericksonls.com>
2023-10-15 16:04:58 -0700

    finished horrible pseudocode

--- main.rb
+++ main.rb
@@ -1,18 +1,47 @@
 class Student
-
-
-
-
-  MAX_COURSES = 5
-  def remove_from_schedule(course, schedule, quarter_id)
-    schedule.quarter[quarter_id].course_list.remove(course)
-  end
-  def add_to_schedule(course, schedule, quarter_id)
-    quarter     = schedule.quarter[quarter_id]
-    course_list = quarter.course_list
-    max_courses = course_list.maximum_number_of_courses
-    if max_courses < MAX_COURSES
-        course_list.add(course)
+    @MAX_COURSES = 5
+    @schedule
+    def add_course(course, quarter, max)
+        schedule.add_course(course, quarter, max)
     end
+    def remove_course(course)
+        schedule.remove_course(course)
     end
+end
+
+class Schedule
+    []Quarters term;
+    def add_course(course, quarter)
+        if term[quarter].class_count > max {
+            throw error;
+        }else{
+            term[quarter].add_course(course);
+        }
+        
+    end
+    
+    def remove_course(course)
+        quarters[quarter].remove_course(course);
+    end
+
+end
+
+class Quarter
+    []Course classes
+    
+    def class_count()
+      classes.length
+    end
+    def add_course(course)
+        classes.push(course)
+    end
+    
+    def remove_course(course)
+        classes.remove(course)
+    end
+
+end
+class Course
+    @name
+    @id
 end
