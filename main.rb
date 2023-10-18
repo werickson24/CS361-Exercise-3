9ae32f4849d72b39922edc0f7ce05a26cacc4105
Wil Erickson <william@ericksonls.com>
2023-10-18 16:33:32 -0700

    structual changes started, temporary header added

--- main.rb
+++ main.rb
@@ -1,11 +1,12 @@
+#RESTRUCTURING IN PROGRESS
 class Student
     MAX_COURSES = 5
     schedule
     def initialize()
         schedule = Schedule.new
     end
-    def add_course(course, quarter, max)
-        schedule.add_course(course, quarter, max)
+    def add_course(course, max = MAX_COURSES)
+        schedule.add_course(course, max)
     end
     def remove_course(course)
         schedule.remove_course(course)
@@ -17,9 +18,9 @@ class Schedule
     def initialize()
         term = Array.new
     end
-    def add_course(course, quarter)
-        if term[quarter].class_count > max {
-            throw error;
+    def add_course(course, max_count)
+        if term[quarter].class_count > max_max {
+            return error;
         }else{
             term[quarter].add_course(course);
         }
