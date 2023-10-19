0bcb712505b8b5a3e87563a089e18347d5e05601
Wil Erickson <william@ericksonls.com>
2023-10-18 22:50:34 -0700

    refactored class methods and logic flow, and corrected to proper ruby

--- main.rb
+++ main.rb
@@ -1,58 +1,79 @@
-#RESTRUCTURING IN PROGRESS
 class Student
+
     MAX_COURSES = 5
     schedule
+    
     def initialize()
         schedule = Schedule.new
     end
+    
     def add_course(course, max = MAX_COURSES)
         schedule.add_course(course, max)
     end
+    
     def remove_course(course)
         schedule.remove_course(course)
     end
+    
 end
 
 class Schedule
-    term
+
+    quarters
+    
     def initialize()
-        term = Array.new
-    end
-    def add_course(course, max_count)
-        if term[quarter].class_count > max_max {
-            return error;
-        }else{
-            term[quarter].add_course(course);
-        }
-        
+        quarters = [Quarter.new, Quarter.new, Quarter.new, Quarter.new]
     end
     
-    def remove_course(course, quarter)
-        term[quarter].remove_course(course);
+    def add_course(course, max_count)
+        current_quarter = course.quarter;
+        if quarters[current_quarter].can_add_course(max_count) {
+            quarters[current_quarter].add_course(course)
+        }else{
+            puts "Maximum number of courses reached"
+            # throw error
+        }
+    end
+    
+    def remove_course(course)
+        current_quarter = course.quarter;
+        quarters[current_quarter].remove_course(course)
     end
 
 end
 
 class Quarter
+
     classes
     
     def initialize()
         classes = Array.new
     end
-    def class_count()
-        classes.size
-    end
+    
     def add_course(course)
         classes.push(course)
     end
     
     def remove_course(course)
-        classes.remove(course)
+        classes.delete(course)
+    end
+    
+    def class_count()
+        classes.size
+    end
+    
+    def can_add_course(max_count)
+        max_count > class_count
     end
 
 end
+
 class Course
-    id
-    name
+
+    def initialize(name, quarter)
+        @name = name
+        @quarter = quarter
+        # More info in the future...
+    end
     
 end
